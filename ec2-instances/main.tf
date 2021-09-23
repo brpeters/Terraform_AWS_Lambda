3f2295061e9d962c9ff3cb3ca0d2a4aa1bf8f80c
Bruno Peters <brunopeters@pop-os.localdomain>
2021-09-22 22:44:17 -0300

    corrigindo main.tf

--- ec2-instances/main.tf
+++ ec2-instances/main.tf
@@ -1,15 +1,6 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
-    }
-  }
-  required_version = ">= 0.14.9"
-}
 
 provider "aws" {
-  region  = "us-east-1"
+  region  = "us-east-2"
 }
 
 resource "aws_security_group" "teste_lambda_instances" {
@@ -37,8 +28,8 @@ resource "aws_security_group_rule" "app_server_sg_outbound" {
 }
 
 resource "aws_instance" "teste_lambda_01" {
-  ami           = "ami-09e67e426f25ce0d7"
-  instance_type = "t3a.small"
+  ami           = "ami-00399ec92321828f5"
+  instance_type = "t2.micro"
   vpc_security_group_ids = [ "${aws_security_group.teste_lambda_instances.id}" ]
   tags = {
     Name = "Teste Lambda 01",
@@ -47,29 +38,11 @@ resource "aws_instance" "teste_lambda_01" {
 }
 
 resource "aws_instance" "teste_lambda_02" {
-  ami           = "ami-09e67e426f25ce0d7"
-  instance_type = "t3a.small"
+  ami           = "ami-00399ec92321828f5"
+  instance_type = "t2.micro"
   vpc_security_group_ids = [ "${aws_security_group.teste_lambda_instances.id}" ]
   tags = {
     Name = "Teste Lambda 02",
     Desliga = "True"
-  }
-}
-
-resource "aws_instance" "teste_lambda_03" {
-  ami           = "ami-09e67e426f25ce0d7"
-  instance_type = "t3a.small"
-  vpc_security_group_ids = [ "${aws_security_group.teste_lambda_instances.id}" ]
-  tags = {
-    Name = "Teste Lambda 03",
-  }
-}
-
-resource "aws_instance" "teste_lambda_04" {
-  ami           = "ami-09e67e426f25ce0d7"
-  instance_type = "t3a.small"
-  vpc_security_group_ids = [ "${aws_security_group.teste_lambda_instances.id}" ]
-  tags = {
-    Name = "Teste Lambda 04",
   }
 }
